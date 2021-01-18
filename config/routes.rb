addbc04aa201c58d0c3bb899e559c999be3186d9
Susan Prestage <susan@sportsrocket.com>
2021-01-17 22:34:41 -0500

    Add pages for each vehicle.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get '/local' => 'home#local'
   get '/about' => 'home#about'
 
+  get '/h1_van' => 'home#h1_van'
+  get '/creta' => 'home#creta'
+  get '/kia_rio_grey' => 'home#kia_rio_grey'
+  get '/mitsubishi' => 'home#mitsubishi'
+
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
